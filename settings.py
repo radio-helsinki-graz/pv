711532a656be891b8e5328ad14c8357462640b06
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2011-03-12 14:56:18 +0100

    fixed name of local sqlite database file.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'helsinki.data',
+        'NAME': 'helsinki.sqlite',
     }
 }
 
