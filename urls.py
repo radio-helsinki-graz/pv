158b1d5c12fd2332960671bce6d71953871da3cf
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2011-04-02 14:32:19 +0200

    added static_media.

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+
+import os
 
 admin.autodiscover()
 
@@ -7,3 +10,10 @@ urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     (r'^program/', include('helsinki.program.urls')),
 )
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$',
+         'django.views.static.serve',
+         {'document_root': os.path.join(os.path.dirname(__file__), 'site_media')}
+        ),
+    )
