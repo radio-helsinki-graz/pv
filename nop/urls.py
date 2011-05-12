3a049a53ac4de64a87911e70f8467e72a4769373
Johannes Raggam <raggam-nl@adm.at>
2011-05-12 18:50:40 +0200

    different url schema for get_current, include nop_form

--- nop/urls.py
+++ nop/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from views import get, get_current
+from views import get, get_current, nop_form
 
 urlpatterns = patterns('',
-    url(r'^$', get_current),
+    url(r'^get_current/?$', get_current),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<hour>\d{1,2})/(?P<minute>\d{1,2})/?$', get),
+    url(r'^$', nop_form),
 )
