ed2890927d79792e9222f61c19969352b614da8f
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2011-06-18 20:43:30 +0200

    fixed week view.

---
+++ program/templatetags/timeslots.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+from datetime import datetime, time, timedelta
+
+@register.simple_tag
+def duration(start, end):
+    return 'style="height: %dpx"' % ((end-start).seconds/60)
+
+@register.simple_tag
+def duration_until(end):
+    start = datetime.combine(end.date(), time(6,0))
+    return 'style="height: %dpx"' % ((end-start).seconds/60)
+
+@register.simple_tag
+def duration_since(start):
+    if start.time()<time(0,0):
+        end = datetime.combine(start.date()+timedelta(days=1), time(6,0))
+    else:
+        end = datetime.combine(start.date(), time(6,0))
+    return 'style="height: %dpx"' % ((end-start).seconds/60)
