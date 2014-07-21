821a356f1a8a36560f6731a716c65b5c80553379
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2014-07-21 21:13:18 +0200

    handle errors better

--- program/utils.py
+++ program/utils.py
@@ -2,12 +2,27 @@ from django.conf import settings
 
 import json
 import urllib
+from os.path import join
 
 
 def get_automation_id_choices():
     base_url = getattr(settings, 'AUTOMATION_BASE_URL', None)
+    cache_dir = getattr(settings, 'AUTOMATION_CACHE_DIR', 'cache')
+    cached_shows = join(cache_dir, 'shows.json')
     shows = []
     if base_url:
-        shows_list = json.load(urllib.urlopen(base_url))['shows']
+        try:
+            shows_json = urllib.urlopen(base_url).read()
+            shows_list = json.loads(shows_json)['shows']
+        except IOError:
+            try:
+                with open(cached_shows) as cache:
+                    shows_list = json.loads(cache.read())['shows']
+            except IOError:
+                shows_list = []
+        else:
+            with open(cached_shows, 'w') as cache:
+                cache.write(shows_json)
+
         shows = [(s['id'], s['title']) for s in shows_list]
     return shows
