97f0d0bcd2ad70630d5eae09ff63e8850538d39e
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2015-12-28 17:33:52 +0100

    reformated code.

--- pv/urls.py
+++ pv/urls.py
@@ -6,17 +6,15 @@ admin.autodiscover()
 
 from program.views import json_day_schedule
 
-urlpatterns = patterns(
-    '',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^program/', include('program.urls')),
-    (r'^nop', include('nop.urls')),
-    (r'^tinymce/', include('tinymce.urls')),
-    url(r'^export/day_schedule/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
-        json_day_schedule))
+urlpatterns = patterns('',
+                       (r'^admin/', include(admin.site.urls)),
+                       (r'^program/', include('program.urls')),
+                       (r'^nop', include('nop.urls')),
+                       (r'^tinymce/', include('tinymce.urls')),
+                       url(r'^export/day_schedule/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', json_day_schedule)
+)
 
 if settings.DEBUG:
-    urlpatterns += patterns(
-        '',
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT}))
+    urlpatterns += patterns('',
+                            (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+)
