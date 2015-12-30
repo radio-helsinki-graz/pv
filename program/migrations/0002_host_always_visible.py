87c905dbcec3d8c8587c832ed11abe1dc9fb8848
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2015-12-30 09:42:12 +0100

    added migrations for nop & program

---
+++ program/migrations/0002_host_always_visible.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('program', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='host',
+            name='always_visible',
+            field=models.BooleanField(default=False, verbose_name='Always visible'),
+        ),
+    ]
