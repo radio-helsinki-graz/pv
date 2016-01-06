54611a97d636238e9bba65e6d96690ac1446de09
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2016-01-06 19:59:46 +0100

    renamed model field

--- program/migrations/0002_host_is_always_visible.py
+++ program/migrations/0002_host_is_always_visible.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.AddField(
             model_name='host',
-            name='always_visible',
-            field=models.BooleanField(default=False, verbose_name='Always visible'),
+            name='is_always_visible',
+            field=models.BooleanField(default=False, verbose_name='Is always visible'),
         ),
     ]
