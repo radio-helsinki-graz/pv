f4edd054b264ffe0002dad2ef41bf45a95e4928a
Ernesto Rico-Schmidt <e.rico.schmidt@gmail.com>
2016-01-06 20:04:39 +0100

    moved tofirstdayinisoweek to utils

--- program/utils.py
+++ program/utils.py
@@ -3,6 +3,7 @@ from django.conf import settings
 import json
 import urllib
 from os.path import join
+from datetime import datetime, date, timedelta
 
 
 def get_automation_id_choices():
@@ -26,3 +27,11 @@ def get_automation_id_choices():
 
         shows = [(s['id'], s['title']) for s in shows_list]
     return shows
+
+
+def tofirstdayinisoweek(year, week):
+    # http://stackoverflow.com/questions/5882405/get-date-from-iso-week-number-in-python
+    ret = datetime.strptime('%04d-%02d-1' % (year, week), '%Y-%W-%w')
+    if date(year, 1, 4).isoweekday() > 4:
+        ret -= timedelta(days=7)
+    return ret
