6990ff7f88f827d7193cb5ddef0be22546bccd23
Ernesto Rico-Schmidt <ernesto.rico-schmidt@evolaris.net>
2016-08-16 17:51:35 +0200

    simplified update commands

--- program/management/commands/update_programslots.py
+++ program/management/commands/update_programslots.py
@@ -2,23 +2,14 @@ from django.core.management.base import NoArgsCommand
 
 from program.models import ProgramSlot
 
-from datetime import date
+from datetime import datetime
 
 
 class Command(NoArgsCommand):
     help = 'update programslots by setting is_active'
 
     def handle_noargs(self, **options):
-        activated = 0
-        deactivated = 0
-
-        for programslot in ProgramSlot.objects.all():
-            programslot.is_active = programslot.until > date.today()
-            programslot.save()
-
-            if programslot.is_active:
-                activated += 1
-            else:
-                deactivated += 1
+        deactivated = ProgramSlot.objects.filter(until__lt=datetime.now()).update(is_active=False)
+        activated = ProgramSlot.objects.filter(until__gt=datetime.now()).update(is_active=True)
 
         print "%s program slots activated, %s program slots de-activated" % (activated, deactivated)
