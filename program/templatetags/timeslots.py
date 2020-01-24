943ce9db0959edee22dcdc6f613421862e345216
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2020-01-23 23:53:48 -0400

    more height to display the 5 min slot at 12 pm beginning next week

--- program/templatetags/timeslots.py
+++ program/templatetags/timeslots.py
@@ -6,20 +6,23 @@ register = template.Library()
 
 
 @register.simple_tag
-def duration(start, end):
-    return 'style="height: %dpx"' % ((end-start).seconds/60)
+def height(start, end):
+    if start.year == 2020 and int(start.strftime('%V')) >= 5 and start.hour == 12 and start.minute == 0:
+        return '30'
+    else:
+        return '%d' % ((end - start).seconds / 60)
 
 
 @register.simple_tag
-def duration_until(end):
+def height_until(end):
     start = datetime.combine(end.date(), time(6, 0))
-    return 'style="height: %dpx"' % ((end-start).seconds/60)
+    return '%d' % ((end - start).seconds / 60)
 
 
 @register.simple_tag
-def duration_since(start):
+def height_since(start):
     if start.time() < time(23, 59):
-        end = datetime.combine(start.date()+timedelta(days=1), time(6, 0))
+        end = datetime.combine(start.date() + timedelta(days=1), time(6, 0))
     else:
         end = datetime.combine(start.date(), time(6, 0))
-    return 'style="height: %dpx"' % ((end-start).seconds/60)
+    return '%d' % ((end - start).seconds / 60)
