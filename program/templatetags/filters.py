850f92794a2571f44fd95f53a3f96f5ea659b3a4
Christian Pointner <equinox@helsinki.at>
2021-02-26 19:28:04 +0100

    initial cleanup of V2 templates

--- program/templatetags/filters.py
+++ program/templatetags/filters.py
@@ -5,26 +5,26 @@ from program.models import BroadcastFormat, MusicFocus, ShowInformation, ShowTop
 register = template.Library()
 
 
-@register.inclusion_tag('boxes/broadcastformat.html')
-def broadcastformat():
+@register.inclusion_tag('v2/filters/broadcastformat.html')
+def broadcastformatV2():
     return {'broadcastformat_list': BroadcastFormat.objects.filter(enabled=True)}
 
 
-@register.inclusion_tag('boxes/musicfocus.html')
-def musicfocus():
+@register.inclusion_tag('v2/filters/musicfocus.html')
+def musicfocusV2():
     return {'musicfocus_list': MusicFocus.objects.all()}
 
 
-@register.inclusion_tag('boxes/showinformation.html')
-def showinformation():
+@register.inclusion_tag('v2/filters/showinformation.html')
+def showinformationV2():
     return {'showinformation_list': ShowInformation.objects.all()}
 
 
-@register.inclusion_tag('boxes/showtopic.html')
-def showtopic():
+@register.inclusion_tag('v2/filters/showtopic.html')
+def showtopicV2():
     return {'showtopic_list': ShowTopic.objects.all()}
 
 
-@register.inclusion_tag('boxes/language.html')
-def language():
+@register.inclusion_tag('v2/filters/language.html')
+def languageV2():
     return {'language_list': Language.objects.all()}
